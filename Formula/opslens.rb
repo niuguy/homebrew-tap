d4eb702a5eb39a80350363a22ed11a6cc8a9683e
Feng <tf.wang.seu@gmail.com>
2026-08-01 08:13:58 +0100

    Make OpsLens formula audit-clean

--- Formula/opslens.rb
+++ Formula/opslens.rb
@@ -1,7 +1,6 @@
 class Opslens < Formula
   desc "Local-first operational incident investigation CLI"
   homepage "https://github.com/niuguy/opslens"
-  version "0.1.1"
   license "MIT"
 
   on_macos do
